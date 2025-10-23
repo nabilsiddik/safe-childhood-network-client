2d21bdd4b035396de6a12d4e1f2377d343f103de
nabilsiddik <nabilsiddik90@gmail.com>
2025-10-24 00:33:28 +0600

    replace userid with email

--- auth.ts
+++ auth.ts
@@ -9,6 +9,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: "jwt",
   },
   callbacks: {
+    async signIn({ user }) {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/user`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            fullName: user.name,
+            email: user.email,
+            profilePhoto: user.image,
+          }),
+        })
+      } catch (err) {
+        console.log('Error while saving user', err)
+        return false;
+      }
+      return true;
+    },
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token
